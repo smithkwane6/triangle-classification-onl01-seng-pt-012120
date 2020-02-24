a20be3ebbf7312752460299daebcbd5ad9691139
Kwane Smith <smithkwane6@gmail.com>
2020-02-24 23:15:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,29 @@
 class Triangle
-  # write code here
-end
-	def initialize ( a,b,c)
-		@a = a
-		@b = b
-		@c = c
-	end
-	def kind
-		if ( @a + @b > @c && @b + @c > @a && @a + @c > @b )
-			if ( @a == @b && @b == @c )
-				return :equilateral
-			elsif ( @a == @b || @a == @c || @b == @c )
-				return :isosceles
-			else
-				return :scalene
-			end
-		else
-			raise TriangleError
-			#begin
-			#	raise TriangleError
-			#rescue TriangleError => error
-			#	error.message
-			#end
-		end
-	end
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
 
 end
 
 class TriangleError < StandardError
-	#def message
-	#	"Some error"
-	#end
+  # triangle error code
 end
